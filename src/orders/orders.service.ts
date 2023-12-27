a611acc6c75345b4aeccbc84287a1ab7b347d53e
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-27 13:00:53 +0100

    Add methods to orders service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Order } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class OrdersService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Order[]> {
+    return this.prismaService.order.findMany();
+  }
+
+  public getById(id: Order['id']): Promise<Order | null> {
+    return this.prismaService.order.findUnique({
+      where: { id },
+    });
+  }
+
+  public deleteById(id: Order['id']): Promise<Order> {
+    return this.prismaService.order.delete({
+      where: { id },
+    });
+  }
+
+  // Omit tworzy nowy interfejs na podstawie Product ale bez "id"
+  public create(productData: Omit<Order, 'id'>): Promise<Order> {
+    return this.prismaService.order.create({
+      data: productData,
+    });
+  }
+
+  public updateById(
+    id: Order['id'],
+    productData: Omit<Order, 'id'>,
+  ): Promise<Order> {
+    return this.prismaService.order.update({
+      where: { id },
+      data: productData,
+    });
+  }
 }
