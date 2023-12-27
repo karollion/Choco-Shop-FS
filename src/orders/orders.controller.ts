641bdf6f171030f6778c64c72ba850a318a78d8c
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-27 13:10:31 +0100

    Add methods to orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  //Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  //Post,
+  //Put,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.ordersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const ord = await this.ordersService.getById(id);
+    if (!ord) throw new NotFoundException('Order not found');
+    return ord;
+  }
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
+      throw new NotFoundException('Order not found');
+    await this.ordersService.deleteById(id);
+    return { success: true };
+  }
+
+  // @Post('/')
+  // create(@Body() orderData: CreateOrderDTO) {
+  //   return this.ordersService.create(orderData);
+  // }
+
+  // @Put('/:id')
+  // async update(
+  //   @Param('id', new ParseUUIDPipe()) id: string,
+  //   @Body() orderData: UpdateOrderDTO,
+  // ) {
+  //   if (!(await this.ordersService.getById(id)))
+  //     throw new NotFoundException('Order not found');
+
+  //   await this.ordersService.updateById(id, orderData);
+  //   return { success: true };
+  // }
 }
