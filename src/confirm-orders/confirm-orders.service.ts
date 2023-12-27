2141fa971c382f084b783ad2e4b7d0c1ee042561
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-27 13:16:29 +0100

    Add methods to confirmOrders service

--- src/confirm-orders/confirm-orders.service.ts
+++ src/confirm-orders/confirm-orders.service.ts
@@ -1,7 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { ConfirmOrder } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class ConfirmOrdersService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<ConfirmOrder[]> {
+    return this.prismaService.confirmOrder.findMany();
+  }
+
+  public getById(id: ConfirmOrder['id']): Promise<ConfirmOrder | null> {
+    return this.prismaService.confirmOrder.findUnique({
+      where: { id },
+    });
+  }
+
+  public deleteById(id: ConfirmOrder['id']): Promise<ConfirmOrder> {
+    return this.prismaService.confirmOrder.delete({
+      where: { id },
+    });
+  }
+
+  // Omit tworzy nowy interfejs na podstawie Product ale bez "id" !!!
+  public create(
+    confirmOrderData: Omit<ConfirmOrder, 'id'>,
+  ): Promise<ConfirmOrder> {
+    return this.prismaService.confirmOrder.create({
+      data: confirmOrderData,
+    });
+  }
+
+  public updateById(
+    id: ConfirmOrder['id'],
+    confirmOrderData: Omit<ConfirmOrder, 'id'>,
+  ): Promise<ConfirmOrder> {
+    return this.prismaService.confirmOrder.update({
+      where: { id },
+      data: confirmOrderData,
+    });
+  }
 }
