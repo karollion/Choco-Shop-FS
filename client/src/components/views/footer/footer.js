c695cf68add6ff7412c99cf620a17c4c2d8e67f1
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-01-13 22:44:20 +0100

    add info to footer

--- client/src/components/views/footer/footer.js
+++ client/src/components/views/footer/footer.js
@@ -1,3 +1,4 @@
+import Container from '../../common/container/Container';
 import styles from './footer.module.scss';
 const Footer = () => {
   
@@ -9,7 +10,54 @@ const Footer = () => {
 
     return (
       <footer className={styles.root}>
-        <p>Copyright &copy; Choco-Shop {getDate()}</p>
+        <Container>
+          <div className={styles.footercontent}>
+            <div className={styles.footersection}>
+              <h4>About Us</h4>
+              <p>Discover our story and mission.</p>
+              <a href="/about">Learn more</a>
+            </div>
+
+            <div className={styles.footersection}>
+              <h4>Help</h4>
+              <ul>
+                <li><a href="/help/orders">Order Status</a></li>
+                <li><a href="/help/returns">Returns and Exchanges</a></li>
+                <li><a href="/contact">Contact Us</a></li>
+                <li><a href="/help/faq">FAQs</a></li>
+              </ul>
+            </div>
+
+            <div className={styles.footersection}>
+              <h4>Follow Us</h4>
+              <ul>
+                <li><a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a></li>
+                <li><a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a></li>
+                <li><a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a></li>
+                <li><a href="https://www.pinterest.com" target="_blank" rel="noreferrer">Pinterest</a></li>
+              </ul>
+            </div>
+
+            <div className={styles.footersection}>
+              <h4>Customer Service</h4>
+              <ul>
+                <li><a href="/customer-service/shipping">Shipping Information</a></li>
+                <li><a href="/customer-service/privacy">Privacy Policy</a></li>
+                <li><a href="/customer-service/terms">Terms and Conditions</a></li>
+              </ul>
+            </div>
+
+            <div className={styles.footersection}>
+              <h4>Newsletter</h4>
+              <p>Subscribe to our newsletter.</p>
+              <a href="/newsletter-subscribe">Subscribe Now</a>
+            </div>
+          </div>
+
+          <div className={styles.footerbottom}>
+            <p>Copyright &copy; Choco-Shop {getDate()}</p>
+          </div>
+        </Container>
       </footer>
     );
 };
