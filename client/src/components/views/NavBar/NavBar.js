81c7c970cbde78f96a50b4bfce8ccf7c338b6725
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-01-18 21:07:46 +0100

    Add RWD styles to navbar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -7,26 +7,28 @@ import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { getUser } from '../../../redux/userRedux';
 import { getAllOrders } from '../../../redux/ordersRedux';
+import { useState } from 'react';
 
 const NavBar = () => {
   const user = useSelector((state) => getUser(state));
   const orders = useSelector((state) =>  getAllOrders(state))
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <div className={styles.root}>
       <Container>
-        <Navbar variant="dark" expand="lg" className='mb-4'>
+        <Navbar expanded={expanded} variant="dark" expand="lg" className='mb-4'>
             <Navbar.Brand className={styles.logo} as={NavLink} to="/">Choco-Shop</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className={styles.nav} id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/">Home</NavLink>
-                <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/about">About</NavLink>
-                <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/contact">Contact</NavLink>
-                {!user ? (<NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/login">Login</NavLink>) : null }
-                {!user ? (<NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/signup">Sign up</NavLink>) : null }
-                {user ? (<NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/logout">Logout</NavLink>) : null }
-                <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/cart">
+                <NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/">Home</NavLink>
+                <NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/about">About</NavLink>
+                <NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/contact">Contact</NavLink>
+                {!user ? (<NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/login">Login</NavLink>) : null }
+                {!user ? (<NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/signup">Sign up</NavLink>) : null }
+                {user ? (<NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/logout">Logout</NavLink>) : null }
+                <NavLink onClick={() => setExpanded(false)} className={({ isActive }) => isActive ? styles.linkActive : undefined} as={NavLink} to="/cart">
                   <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
                   <div className={styles.cartOrders}>{orders.length}</div>
                 </NavLink>
