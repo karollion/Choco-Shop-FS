69261f1b608da09379d573204c050cf24d723c50
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-01-21 20:33:53 +0100

    Add changes prices depend of sizes

--- client/src/components/common/SumOrders/SumOrders.js
+++ client/src/components/common/SumOrders/SumOrders.js
@@ -7,12 +7,29 @@ import { getAllProducts } from '../../../redux/productsRedux';
 const SumOrders = ({ orders }) => {
   const products = useSelector(getAllProducts);
   let delivery = 0;
-
   let sumPrice = 0;
+
+  const calcPriceOfSize = (size) => {
+    switch (size) {
+      case 'S':
+        return 1;
+      case 'M':
+        return 1.3;
+      case 'L':
+        return 1.5;
+      case 'XL':
+        return 1.8;
+      case 'XXL':
+        return 2;
+      default:
+        return 1;
+    }
+  };
+
   for(let order of orders) {
     for(let product of products){
       if(order.productId === product.id) {
-        sumPrice = sumPrice + (order.quantity * product.price);
+        sumPrice = sumPrice + (order.quantity * product.price * calcPriceOfSize(order.size));
         break;
       }
     }
